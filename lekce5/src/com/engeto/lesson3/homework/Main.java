0ad50a59d4bc9fd2475c9a2a569b3ce78ad3d5ca
Roman Varadi <roman@varadi.cz>
2021-08-15 22:21:17 +0200

    Domaci ukol je cca z jedne poloviny hotovy.

--- lekce5/src/com/engeto/lesson3/homework/Main.java
+++ lekce5/src/com/engeto/lesson3/homework/Main.java
@@ -3,6 +3,15 @@ package com.engeto.lesson3.homework;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Flower flower1 = null;
+        try {
+            flower1 = new Flower("Fuchsie");
+        } catch (PlantException e) {
+            e.printStackTrace();
+        }
+
+        if (flower1 != null) {
+            System.out.println(flower1.getWateringInfo());
+        }
     }
 }
---
+++ lekce5/src/com/engeto/lesson3/homework/PlantException.java
@@ -0,0 +1,7 @@
+package com.engeto.lesson3.homework;
+
+public class PlantException extends Exception{
+    public PlantException( String message) {
+        super(message);
+    }
+}
