20e47bab7a08bec775f4f281819e66ba16703df0
Roman Varadi <roman@varadi.cz>
2021-08-15 20:18:47 +0200

    ..

---
+++ lekce4/vyjimky_pokusy/src/com/engeto/lesson3/homework/Main.java
@@ -0,0 +1,8 @@
+package com.engeto.lesson3.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int c = 1 / 1;
+    }
+}
---
+++ lekce5/src/com/engeto/lesson3/homework/Main.java
@@ -0,0 +1,8 @@
+package com.engeto.lesson3.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
