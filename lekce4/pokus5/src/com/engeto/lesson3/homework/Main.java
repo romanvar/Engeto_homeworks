4d0f06c9dfd0e00c9e89f67f255d35e250292b40
romanvar <roman@varadi.cz>
2021-08-19 19:51:37 +0200

    Delete lekce4 directory

--- lekce4/pokus5/src/com/engeto/lesson3/homework/Main.java
+++
@@ -1,8 +0,0 @@
-package com.engeto.lesson3.homework;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("nazdar");
-    }
-}
--- lekce4/vyjimky_pokusy/src/com/engeto/lesson3/homework/Main.java
+++
@@ -1,8 +0,0 @@
-package com.engeto.lesson3.homework;
-
-public class Main {
-
-    public static void main(String[] args) {
-        int c = 1 / 1;
-    }
-}
