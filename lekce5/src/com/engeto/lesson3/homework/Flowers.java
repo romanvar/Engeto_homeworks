0feb07225c2620c04163e7ffea64ac8d266d9dac
romanvar <roman@varadi.cz>
2021-08-19 19:35:12 +0200

    Delete lekce5/src/com/engeto/lesson3/homework directory

--- lekce5/src/com/engeto/lesson3/homework/Flowers.java
+++
@@ -1,9 +0,0 @@
-package com.engeto.lesson3.homework;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Flowers {
-    public static final String DELIMITER = "\t";
-    List<Flower> list = new ArrayList<>();
-}
--- lekce5/src/com/engeto/lesson3/homework/PlantException.java
+++
@@ -1,7 +0,0 @@
-package com.engeto.lesson3.homework;
-
-public class PlantException extends Exception{
-    public PlantException( String message) {
-        super(message);
-    }
-}
