4024eb96b52687128bfb3a058d73bbaa13ae5eef
roman <roman@varadi.cz>
2021-08-20 17:10:47 +0200

    homework_lesson5 - final version

--- lesson5/src/com/engeto/lesson5/Flowers.java
+++ lesson5/src/com/engeto/lesson5/Flowers.java
@@ -1,9 +1,74 @@
 package com.engeto.lesson5;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Flowers {
     public static final String DELIMITER = "\t";
-    List<Flower> list = new ArrayList<>();
+    private List<Flower> listFlowers = new ArrayList<>();
+
+    public static Flowers importFromTextFile(String filename) throws PlantException {
+        Flowers flowers = new Flowers();
+        try (Scanner scanner = new Scanner(new FileInputStream(filename))) {
+            while (scanner.hasNextLine()) {
+                String inputline = scanner.nextLine();
+               // System.out.println("Byla nactena jedna radka "+inputline);
+                String[] items = inputline.split(DELIMITER);
+                if (items.length != 5)
+                    throw new PlantException("Nespravny pocet polozek na radku: " + inputline + " (" + items.length + " položek)"
+                    );
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate parsedDatePlanted = LocalDate.parse(items[4], formatter);
+                LocalDate parsedDateLastWatering = LocalDate.parse(items[3], formatter);
+                int i = Integer.parseInt(items[2]);
+                System.out.println(" datum zalivky je: "+items[4]);
+
+                //Flower flower = new Flower(items[0], items[1], parsedDatePlanted, parsedDateLastWatering, i);
+                flowers.addFlower(new Flower(items[0], items[1], parsedDatePlanted, parsedDateLastWatering, i));
+
+            }
+
+        } catch (FileNotFoundException ex) {
+            throw new PlantException("Soubor " + filename + " nenalezen "  + ex.getLocalizedMessage());
+        } catch (Exception ex) {
+            throw new PlantException("Some Exception: " + ex.getLocalizedMessage());
+        }
+
+
+        return flowers;
+    }
+
+    public void exportToFile(String filename) throws PlantException {
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(filename))) {
+            for (Flower flower : listFlowers) {
+                writer.println(
+                        flower.getName() + DELIMITER
+                                + flower.getNotes() + DELIMITER
+                                + flower.getFrequencyOfWatering()+DELIMITER
+                                + flower.getLastWatering() + DELIMITER
+                                + flower.getPlanted() + DELIMITER
+
+                );
+            }
+        } catch (FileNotFoundException e) {
+            throw new PlantException("Soubor " + filename + " nenalezen: " + e.getLocalizedMessage());
+        }
+    }
+
+
+    public void addFlower(Flower newFlower) {
+        listFlowers.add(newFlower);
+    }
+
+    public void removeFlower(Flower newFlower) {
+        listFlowers.remove(newFlower);
+    }
+
 }
--- lesson5/src/com/engeto/lesson5/Main.java
+++ lesson5/src/com/engeto/lesson5/Main.java
@@ -1,17 +1,45 @@
 package com.engeto.lesson5;
 
+import java.io.FileInputStream;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
+    public static final String INPUTFILENAME = "kvetiny.txt";
+    public static final String OUTPUTFILENAME = "vystup.txt";
 
     public static void main(String[] args) {
-        Flower flower1 = null;
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("Current dir using System:" + currentDir);
+
+        Flower flower1 = null, flower2 = null, flower3 = null;
         try {
             flower1 = new Flower("Fuchsie");
+            flower2 = new Flower("Tulipan");
+            flower3 = new Flower("Pampeliska");
             System.out.println(flower1.getWateringInfo());
             flower1.setLastWatering(LocalDate.now());
             System.out.println("Datum sazeni "+flower1.getPlanted());
+          //  Flowers.addFlower(flower1);
         } catch (PlantException e) {
+            e.printStackTrace();
+        }
+        Flowers flowers = new Flowers();
+        flowers.addFlower(flower1);
+        try{
+            flowers = Flowers.importFromTextFile(INPUTFILENAME);
+            flowers.addFlower(flower1);
+            flowers.addFlower(flower2);
+            flowers.removeFlower(flower1);
+            flowers.addFlower(flower3);
+
+        }catch (PlantException e){
+            e.printStackTrace();
+        }
+
+        try{
+            flowers.exportToFile(OUTPUTFILENAME);
+        }catch (PlantException e){
             e.printStackTrace();
         }
 
