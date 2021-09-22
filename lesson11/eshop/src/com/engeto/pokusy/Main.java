d520aed6e7c844b3327868a3491905a729c8ddf3
roman <roman@varadi.cz>
2021-09-22 20:48:46 +0200

    connection to db established

--- lesson11/eshop/src/com/engeto/pokusy/Main.java
+++ lesson11/eshop/src/com/engeto/pokusy/Main.java
@@ -1,8 +1,15 @@
 package com.engeto.pokusy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+        System.out.println("ahoj");
+        Connection dbconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_eshop","my_eshop_user","Ferda123.");
+        System.out.println("Connection to db was established");
+        dbconnection.close();
     }
 }
