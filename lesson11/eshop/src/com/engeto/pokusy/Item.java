132194ac77ccd407d6a379c9a0f6f4c9d955a90e
roman <roman@varadi.cz>
2021-09-22 22:34:23 +0200

    basic sql query created

---
+++ lesson11/eshop/src/com/engeto/pokusy/Item.java
@@ -0,0 +1,75 @@
+package com.engeto.pokusy;
+
+import java.math.BigDecimal;
+
+public class Item {
+    private Integer id;
+
+    private String partNo;
+
+    private String serialNo;
+
+    private String name;
+
+    private String description;
+
+    private Integer numberInStock;
+
+    private BigDecimal price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPartNo() {
+        return partNo;
+    }
+
+    public void setPartNo(String partNo) {
+        this.partNo = partNo;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberInStock() {
+        return numberInStock;
+    }
+
+    public void setNumberInStock(Integer numberInStock) {
+        this.numberInStock = numberInStock;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
--- lesson11/eshop/src/com/engeto/pokusy/Main.java
+++ lesson11/eshop/src/com/engeto/pokusy/Main.java
@@ -1,15 +1,20 @@
 package com.engeto.pokusy;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         System.out.println("ahoj");
-        Connection dbconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_eshop","my_eshop_user","Ferda123.");
+        Connection dbconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_eshop", "my_eshop_user", "Ferda123.");
         System.out.println("Connection to db was established");
+        Statement statement = dbconnection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM my_eshop.ITEM;");
+        while(resultSet.next()){
+        System.out.println("Jmeno je:" + resultSet.getNString(4));
+            System.out.println("Description je: "+resultSet.getNString("description"));
+        System.out.println("Cena je:" + resultSet.getBigDecimal("price"));
+    }
         dbconnection.close();
     }
 }
