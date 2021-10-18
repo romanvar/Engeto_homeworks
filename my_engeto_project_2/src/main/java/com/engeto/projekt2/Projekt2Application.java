ec45bf9c8e12cf03dbcfdcdf855ab08098c0d654
roman <roman@varadi.cz>
2021-10-18 19:34:03 +0200

    JSON parsing added

---
+++ my_engeto_project_2/src/main/java/com/engeto/projekt2/Projekt2Application.java
@@ -0,0 +1,13 @@
+package com.engeto.projekt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt2Application.class, args);
+	}
+
+}
