5b148069ea797449d54a321b0623edd1fa1962b5
roman <roman@varadi.cz>
2021-10-19 22:52:50 +0200

    added client for downloading and parsing of JSON

---
+++ my_engeto_project2/src/main/java/com/engeto/my_engeto_project2/TestMain.java
@@ -0,0 +1,43 @@
+package com.engeto.my_engeto_project2;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
+
+public class TestMain {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TestMain testMain   = new TestMain();
+       String body =  testMain.callApi();
+       testMain.mapToObject(body);
+        System.out.println("ahoj");
+    }
+
+    public String callApi() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("https://euvatrates.com/rates.json")).GET().build();
+       HttpResponse<String> httResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status Code: "+httResponse.statusCode());
+        System.out.println("Body : "+httResponse.body());
+        return httResponse.body();
+
+
+    }
+    public VatResponse mapToObject(String body) throws JsonProcessingException {
+        ObjectMapper objectMapper =new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        VatResponse vatResponse = objectMapper.readValue(body,VatResponse.class);
+        System.out.println("Rates count: "+vatResponse.getRates().size());
+        return vatResponse;
+
+    }
+}
---
+++ my_engeto_project2/src/main/java/com/engeto/my_engeto_project2/VatRate.java
@@ -0,0 +1,22 @@
+package com.engeto.my_engeto_project2;
+
+public class VatRate  {
+public String country;
+public Double rate;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+}
