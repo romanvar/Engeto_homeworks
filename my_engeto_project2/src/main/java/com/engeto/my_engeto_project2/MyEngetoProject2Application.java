617b8629855303c80128bb32349d7b6328377829
roman <roman@varadi.cz>
2021-10-22 16:57:21 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	my_engeto_project2/src/main/java/com/engeto/my_engeto_project2/TestMain.java
    #	my_engeto_project2/src/main/java/com/engeto/my_engeto_project2/VatResponse.java

---
+++ my_engeto_project2/src/main/java/com/engeto/my_engeto_project2/MyEngetoProject2Application.java
@@ -0,0 +1,13 @@
+package com.engeto.my_engeto_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEngetoProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEngetoProject2Application.class, args);
+	}
+
+}
